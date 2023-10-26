48f67eeb14ebaacb3cd107ab2bb3127c162e1603
Anthony Jack <jackie2002jack@gmail.com>
2023-10-26 14:53:46 -0500

    Documentation
    Adding more comments just to make the code more READABLE

--- static/app.js
+++ static/app.js
@@ -1,25 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
+    //Establish a WebSocket Connection to the server
     const socket = io.connect()
+
+    // Create a Chart.js chart for displaying temperature data
     const chart = new Chart(document.getElementById("chart"), {
         type: 'line',
         data: {
-            labels: [],
+            labels: [], // X-axis labels(time)
             datasets: [{
                 label: 'Temperature',
-                data: [],
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
+                data: [], //Y-axis data (tempature values)
+                borderColor: 'rgba(75, 192, 192, 1)', // Line color
+                borderWidth: 1 //Line width
             }]
         },
         options: {
             scales: {
                 y: {
-                    min: 0
+                    min: 0 // Minium value for the Y-axis
                 }
             }
         }
     });
-
+    // Function to get the current time as a formatted string 
     function getCurrentTime() {
         const now = new Date()
         const hours = now.getHours().toString().padStart(2, '0')
@@ -29,26 +32,33 @@ document.addEventListener('DOMContentLoaded', function() {
         return `${hours}:${minutes}:${seconds}`
     }
 
+    //Event handlers for WebSocket events
+    //Connecting event
     socket.on("connect", () => {
         console.log("Socket connection to server successful.")
     })
 
+    //Disconecting
     socket.on("disconnect", (msg) => {
         console.log("Socket disconnected, reason: " + msg)
     })
 
+    //Connection error
     socket.on("connect_error", (msg) => {
         console.log('Error while connecting socket to server, reason: ' + msg)
     })
 
     socket.on("serial", (data) => {
+        //Add the current time to the X-axis Labels and Tempature data to the chart
         chart.data.labels.push(getCurrentTime())
         chart.data.datasets[0].data.push(data.temperature)
 
+        //Limit the number of data points displayed to 10
         if (chart.data.labels.length > 10) {
             chart.data.labels.shift()
             chart.data.datasets[0].data.shift()
         }
+        //update chart
         chart.update()
         // console.log("Data received from server: " + JSON.stringify(data))
         // document.getElementById('temperature').innerText = `Temperature from ESP-32 is ${data.temperature}`
