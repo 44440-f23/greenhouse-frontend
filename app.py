54f379dbe49e19c32666349271be6ec2fcf00957
Avery Schreiner <schreineravery@gmail.com>
2023-10-26 23:09:29 -0500

    add requirements, comments, and readme

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 import serial, json
 
+# init our Flask app and SocketIO
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins='*')
 
+# helper function that reads the COM5 serial port 
 def read_serial():
     serial_port = serial.Serial('COM5', baudrate=9600)
     while True:
+        # serial is wacky, sometimes it doesn't know how to decode() so this is just how it is for now
         try:
             data = serial_port.readline().decode().strip()
             parsed = json.loads(data)
@@ -18,10 +21,12 @@ def read_serial():
             parsed = json.loads(data)
             socketio.emit("serial", parsed)
 
+# root
 @app.route('/')
 def index():
     return render_template('index.html', title="Flaskaroo")
 
+# when the client socket connects
 @socketio.on("connect")
 def connect():
     print("\nSocket connection to client successful.\n")
--- static/app.js
+++ static/app.js
@@ -1,5 +1,11 @@
+// only runs our js code after the page has loaded
 document.addEventListener('DOMContentLoaded', function() {
+    // create our socket 
+    // https://socket.io/
     const socket = io.connect()
+
+    // our nice line chart
+    // https://www.chartjs.org/
     const chart = new Chart(document.getElementById("chart"), {
         type: 'line',
         data: {
@@ -11,15 +17,16 @@ document.addEventListener('DOMContentLoaded', function() {
                 borderWidth: 1
             }]
         },
-        options: {
-            scales: {
-                y: {
-                    min: 0
-                }
-            }
-        }
+        // options: {
+        //     scales: {
+        //         y: {
+        //             min: 0
+        //         }
+        //     }
+        // }
     });
 
+    // a helper function that gets the current time for us in a nice format for our x values
     function getCurrentTime() {
         const now = new Date()
         const hours = now.getHours().toString().padStart(2, '0')
@@ -29,6 +36,7 @@ document.addEventListener('DOMContentLoaded', function() {
         return `${hours}:${minutes}:${seconds}`
     }
 
+    // event listeners that come with socket io 
     socket.on("connect", () => {
         console.log("Socket connection to server successful.")
     })
@@ -41,15 +49,23 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('Error while connecting socket to server, reason: ' + msg)
     })
 
+    // our custom event that is emitted from out app.py
     socket.on("serial", (data) => {
+        // add the current time to the list of X labels
         chart.data.labels.push(getCurrentTime())
+
+        // add the temperature value to the list of Ys
         chart.data.datasets[0].data.push(data.temperature)
 
+        // keeps our chart at 10 x y pairs
         if (chart.data.labels.length > 10) {
             chart.data.labels.shift()
             chart.data.datasets[0].data.shift()
         }
+
+        // show changes
         chart.update()
+
         // console.log("Data received from server: " + JSON.stringify(data))
         // document.getElementById('temperature').innerText = `Temperature from ESP-32 is ${data.temperature}`
         // document.getElementById('humidity').innerText = `Humidity from ESP-32 is ${data.humidity}`
