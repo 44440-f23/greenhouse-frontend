3fccb188ec3b957aaf5baeb12680421a75743d7f
Parker W Christy <pchristy03@gmail.com>
2023-11-18 16:58:14 -0600

    wowza thats a lot

--- static/app.js
+++ static/app.js
@@ -1,92 +1,88 @@
+import { connect } from 'socket.io-client'
+
 // only runs our js code after the page has loaded
-document.addEventListener('DOMContentLoaded', function() {
-    // our greenhouse elements
-    let gh1 = document.getElementById("1")
-    let gh2 = document.getElementById("2")
-    let gh3 = document.getElementById("3")
-    let gh4 = document.getElementById("4")
-    let gh5 = document.getElementById("5")
-    let gh6 = document.getElementById("6")
+document.addEventListener('DOMContentLoaded', function () {
+  // our greenhouse elements
+  const gh1 = document.getElementById('1')
+  const gh2 = document.getElementById('2')
+  const gh3 = document.getElementById('3')
+  const gh4 = document.getElementById('4')
+  const gh5 = document.getElementById('5')
+  const gh6 = document.getElementById('6')
 
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  // const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  // const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-    // create our socket 
-    // https://socket.io/
-    const socket = io.connect()
+  // create our socket
+  // https://socket.io/
+  const socket = connect()
 
-    // event listeners that come with socket io 
-    socket.on("connect", () => {
-        console.log("Socket connection to server successful.")
-    })
+  // event listeners that come with socket io
+  socket.on('connect', () => {
+    console.log('Socket connection to server successful.')
+  })
 
-    //Disconecting
-    socket.on("disconnect", (msg) => {
-        console.log("Socket disconnected, reason: " + msg)
-    })
+  // Disconecting
+  socket.on('disconnect', (msg) => {
+    console.log('Socket disconnected, reason: ' + msg)
+  })
 
-    //Connection error
-    socket.on("connect_error", (msg) => {
-        console.log('Error while connecting socket to server, reason: ' + msg)
-    })
+  // Connection error
+  socket.on('connect_error', (msg) => {
+    console.log('Error while connecting socket to server, reason: ' + msg)
+  })
 
-    // most likely wont be needed in this file, but here for an example/use later
-    socket.on("config", (recieved_configs) => {
-        recieved_configs = JSON.parse(recieved_configs)
-        console.log("These are the current configs: " + JSON.stringify(recieved_configs))
-    })
+  // most likely wont be needed in this file, but here for an example/use later
+  socket.on('config', (recievedConfigs) => {
+    recievedConfigs = JSON.parse(recievedConfigs)
+    console.log('These are the current configs: ' + JSON.stringify(recievedConfigs))
+  })
 
-    // our custom event that is emitted from out app.py
-    socket.on("serial", (data) => {
-        try{
-            data = JSON.parse(data)
-        } catch(e){
-            console.log("failed to parse: " + data + " because: " + e)
-        }
+  // our custom event that is emitted from out app.py
+  socket.on('serial', (data) => {
+    try {
+      data = JSON.parse(data)
+    } catch (e) {
+      console.log('failed to parse: ' + data + ' because: ' + e)
+    }
 
-
-        // check the data id, and add information as necessary
-        if (data.id == 1) {
-            gh1.querySelector(".temp").innerText = parseInt(data.temp) ?? "-"
-            gh1.querySelector(".humidity").innerText = parseInt(data.humidity) ?? "-"
-            // gh1.querySelector(".soil-temp").innerText = parseInt(data.soilT) ?? "-"
-            // gh1.querySelector(".soil-moisture").innerText = parseInt(data.soilM) ?? "-"
-            gh1.querySelector(".lux").innerText = parseInt(data.lightS) ?? "-"
-        }
-        else if (data.id == 2) {
-            gh2.querySelector(".temp").innerText = parseInt(data.temp) ?? "-"
-            gh2.querySelector(".humidity").innerText = parseInt(data.humidity) ?? "-"
-            // gh2.querySelector(".soil-temp").innerText = parseInt(data.soilT) ?? "-"
-            // gh2.querySelector(".soil-moisture").innerText = parseInt(data.soilM) ?? "-"
-            gh2.querySelector(".lux").innerText = parseInt(data.lightS) ?? "-"
-        }
-        else if (data.id == 3) {
-            gh3.querySelector(".temp").innerText = parseInt(data.temp) ?? "-"
-            gh3.querySelector(".humidity").innerText = parseInt(data.humidity) ?? "-"
-            // gh3.querySelector(".soil-temp").innerText = parseInt(data.soilT) ?? "-"
-            // gh3.querySelector(".soil-moisture").innerText = parseInt(data.soilM) ?? "-"
-            gh3.querySelector(".lux").innerText = parseInt(data.lightS) ?? "-"
-        }
-        else if (data.id == 4) {
-            gh4.querySelector(".temp").innerText = parseInt(data.temp) ?? "-"
-            gh4.querySelector(".humidity").innerText = parseInt(data.humidity) ?? "-"
-            // gh4.querySelector(".soil-temp").innerText = parseInt(data.soilT) ?? "-"
-            // gh4.querySelector(".soil-moisture").innerText = parseInt(data.soilM) ?? "-"
-            gh4.querySelector(".lux").innerText = parseInt(data.lightS) ?? "-"
-        }
-        else if (data.id == 5) {
-            gh5.querySelector(".temp").innerText = parseInt(data.temp) ?? "-"
-            gh5.querySelector(".humidity").innerText = parseInt(data.humidity) ?? "-"
-            // gh5.querySelector(".soil-temp").innerText = parseInt(data.soilT) ?? "-"
-            // gh5.querySelector(".soil-moisture").innerText = parseInt(data.soilM) ?? "-"
-            gh5.querySelector(".lux").innerText = parseInt(data.lightS) ?? "-"
-        }
-        else if (data.id == 6) {
-            gh6.querySelector(".temp").innerText = parseInt(data.temp) ?? "-"
-            gh6.querySelector(".humidity").innerText = parseInt(data.humidity) ?? "-"
-            // gh6.querySelector(".soil-temp").innerText = parseInt(data.soilT) ?? "-"
-            // gh6.querySelector(".soil-moisture").innerText = parseInt(data.soilM) ?? "-"
-            gh6.querySelector(".lux").innerText = parseInt(data.lightS) ?? "-"
-        }
-    })
+    // check the data id, and add information as necessary
+    if (data.id === 1) {
+      gh1.querySelector('.temp').innerText = parseInt(data.temp) ?? '-'
+      gh1.querySelector('.humidity').innerText = parseInt(data.humidity) ?? '-'
+      // gh1.querySelector(".soil-temp").innerText = parseInt(data.soilT) ?? "-"
+      // gh1.querySelector(".soil-moisture").innerText = parseInt(data.soilM) ?? "-"
+      gh1.querySelector('.lux').innerText = parseInt(data.lightS) ?? '-'
+    } else if (data.id === 2) {
+      gh2.querySelector('.temp').innerText = parseInt(data.temp) ?? '-'
+      gh2.querySelector('.humidity').innerText = parseInt(data.humidity) ?? '-'
+      // gh2.querySelector(".soil-temp").innerText = parseInt(data.soilT) ?? "-"
+      // gh2.querySelector(".soil-moisture").innerText = parseInt(data.soilM) ?? "-"
+      gh2.querySelector('.lux').innerText = parseInt(data.lightS) ?? '-'
+    } else if (data.id === 3) {
+      gh3.querySelector('.temp').innerText = parseInt(data.temp) ?? '-'
+      gh3.querySelector('.humidity').innerText = parseInt(data.humidity) ?? '-'
+      // gh3.querySelector(".soil-temp").innerText = parseInt(data.soilT) ?? "-"
+      // gh3.querySelector(".soil-moisture").innerText = parseInt(data.soilM) ?? "-"
+      gh3.querySelector('.lux').innerText = parseInt(data.lightS) ?? '-'
+    } else if (data.id === 4) {
+      gh4.querySelector('.temp').innerText = parseInt(data.temp) ?? '-'
+      gh4.querySelector('.humidity').innerText = parseInt(data.humidity) ?? '-'
+      // gh4.querySelector(".soil-temp").innerText = parseInt(data.soilT) ?? "-"
+      // gh4.querySelector(".soil-moisture").innerText = parseInt(data.soilM) ?? "-"
+      gh4.querySelector('.lux').innerText = parseInt(data.lightS) ?? '-'
+    } else if (data.id === 5) {
+      gh5.querySelector('.temp').innerText = parseInt(data.temp) ?? '-'
+      gh5.querySelector('.humidity').innerText = parseInt(data.humidity) ?? '-'
+      // gh5.querySelector(".soil-temp").innerText = parseInt(data.soilT) ?? "-"
+      // gh5.querySelector(".soil-moisture").innerText = parseInt(data.soilM) ?? "-"
+      gh5.querySelector('.lux').innerText = parseInt(data.lightS) ?? '-'
+    } else if (data.id === 6) {
+      gh6.querySelector('.temp').innerText = parseInt(data.temp) ?? '-'
+      gh6.querySelector('.humidity').innerText = parseInt(data.humidity) ?? '-'
+      // gh6.querySelector(".soil-temp").innerText = parseInt(data.soilT) ?? "-"
+      // gh6.querySelector(".soil-moisture").innerText = parseInt(data.soilM) ?? "-"
+      gh6.querySelector('.lux').innerText = parseInt(data.lightS) ?? '-'
+    }
+  })
 })
--- static/chartPage.js
+++ static/chartPage.js
@@ -1,130 +1,130 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const socket = io.connect()
+import { connect } from 'socket.io-client'
+const Chart = require('Chart.js')
 
-    // our selection elements 
-    let gh_select = document.getElementById('gh-select')
-    let variable_select = document.getElementById('variable-select')    
-    let mode_select = document.getElementById('mode-select')
+document.addEventListener('DOMContentLoaded', () => {
+  const socket = connect()
 
-    // their corresponding selected values
-    let gh = Number(gh_select.value)
-    let variable = variable_select.value
-    let mode = mode_select.value
+  // our selection elements
+  const ghSelect = document.getElementById('gh-select')
+  const variableSelect = document.getElementById('variable-select')
+  // const modeSelect = document.getElementById('mode-select')
 
-    // chart details
-    let limit = 10
-    let lastData = 0
-    let data = []
-    let labels = []
-    let title = `${variable_select.options[variable_select.selectedIndex].text} of Greenhouse ${gh}`
-    let chart = new Chart(
-        document.getElementById('chart'),
-        {
-          type: 'line',
-          data: {
-            labels: labels,
-            datasets: [
-              {
-                label: title,
-                data: data
-              }
-            ]
-          },
-          options: {
-            responsive: true,
-            maintainAspectRatio: true
-        }
-        }
-    )
+  // their corresponding selected values
+  let gh = Number(ghSelect.value)
+  let variable = variableSelect.value
+  // let mode = modeSelect.value
 
-    // creates a new blank chart
-    function resetChart() {
-        lastData = 0
-        data = []
-        labels = []
-        title = `${variable_select.options[variable_select.selectedIndex].text} of Greenhouse ${gh}`
-        chart.destroy()
-        chart = new Chart(
-            document.getElementById('chart'),
+  // chart details
+  const limit = 10
+  let lastData = 0
+  let data = []
+  let labels = []
+  let title = `${variableSelect.options[variableSelect.selectedIndex].text} of Greenhouse ${gh}`
+  let chart = new Chart(
+    document.getElementById('chart'),
+    {
+      type: 'line',
+      data: {
+        labels,
+        datasets: [
+          {
+            label: title,
+            data
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: true
+      }
+    }
+  )
+
+  // creates a new blank chart
+  function resetChart () {
+    lastData = 0
+    data = []
+    labels = []
+    title = `${variableSelect.options[variableSelect.selectedIndex].text} of Greenhouse ${gh}`
+    chart.destroy()
+    chart = new Chart(
+      document.getElementById('chart'),
+      {
+        type: 'line',
+        data: {
+          labels,
+          datasets: [
             {
-              type: 'line',
-              data: {
-                labels: labels,
-                datasets: [
-                  {
-                    label: title,
-                    data: data
-                  }
-                ]
-              },
-              options: {
-                responsive: true,
-                maintainAspectRatio: true
+              label: title,
+              data
             }
-            }
-        )
+          ]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: true
+        }
+      }
+    )
+  }
 
-        updateChart()
+  // selection change handlers
+  ghSelect.addEventListener('change', function () {
+    gh = Number(ghSelect.value)
+    resetChart()
+  })
+
+  variableSelect.addEventListener('change', function () {
+    variable = variableSelect.value
+    resetChart()
+  })
+
+  //   modeSelect.addEventListener('change', function () {
+  //     mode = modeSelect.value
+  //     resetChart()
+  //   })
+
+  socket.on('serial', (json) => {
+    json = JSON.parse(json)
+
+    // we are only interested in the data pertaining to the selected greenhouse
+    if (json.id === gh) {
+      switch (variable) {
+        case 'temp':
+          lastData = json.temp
+          break
+        case 'humidity':
+          lastData = json.humidity
+          break
+        case 'soilT':
+          lastData = json.soilT
+          break
+        case 'soilM':
+          lastData = json.soilM
+          break
+        case 'lightS':
+          lastData = json.lightS
+          break
+      }
+      chart.update()
+    }
+  })
+
+  // every second create a new chart entry, regardless of if we received data or not
+  setInterval(() => {
+    const now = new Date()
+    const hours = String(now.getHours()).padStart(2, '0')
+    const minutes = String(now.getMinutes()).padStart(2, '0')
+    const seconds = String(now.getSeconds()).padStart(2, '0')
+
+    // keep chart to past 10 seconds
+    if (labels.length > limit) {
+      labels.shift()
+      data.shift()
     }
 
-
-    // selection change handlers
-    gh_select.addEventListener('change', function () {
-        gh = Number(gh_select.value)
-        resetChart()
-    })
-
-    variable_select.addEventListener('change', function () {
-        variable = variable_select.value
-        resetChart()
-    })
-
-    mode_select.addEventListener('change', function () {
-        mode = mode_select.value
-        resetChart()
-    })    
-
-    socket.on("serial", (json) => {
-        json = JSON.parse(json)
-        
-        // we are only interested in the data pertaining to the selected greenhouse
-        if (json.id == gh) {
-            switch (variable) {
-                case "temp":
-                    lastData = json.temp
-                    break
-                case "humidity":
-                    lastData = json.humidity
-                    break
-                case "soilT":
-                    lastData = json.soilT
-                    break
-                case "soilM":
-                    lastData = json.soilM
-                    break
-                case "lightS":
-                    lastData = json.lightS
-                    break
-            }
-            chart.update()
-        }
-    })
-
-    // every second create a new chart entry, regardless of if we received data or not
-    setInterval(() => {
-        const now = new Date()
-        const hours = String(now.getHours()).padStart(2, '0')
-        const minutes = String(now.getMinutes()).padStart(2, '0')
-        const seconds = String(now.getSeconds()).padStart(2, '0')
-  
-        // keep chart to past 10 seconds
-        if (labels.length > limit) {
-            labels.shift()
-            data.shift()
-        }
-
-        labels.push(`${hours}:${minutes}:${seconds}`)
-        data.push(lastData)
-        chart.update()
-    }, 1000)
+    labels.push(`${hours}:${minutes}:${seconds}`)
+    data.push(lastData)
+    chart.update()
+  }, 1000)
 })
